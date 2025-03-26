b474b27a306424201e74ce9194d5f652b906b5e0
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-03-26 01:19:04 +0100

    FE > Setting up PWA > Setup PWA config information

--- Frontend/MatchMaker/nuxt.config.js
+++ Frontend/MatchMaker/nuxt.config.js
@@ -4,7 +4,13 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  buildModules: ['@nuxtjs/i18n'],
+
+  buildModules: [
+    '@nuxtjs/i18n',
+    '@vite-pwa/nuxt'
+  ],
+  modules: ['@vite-pwa/nuxt'],
+
 
   i18n: {
     detectBrowserLanguage: false,
@@ -24,4 +30,61 @@ export default defineNuxtConfig({
     defaultLocale: 'en-EN',
     langDir: 'locales',
   },
+
+  pwa: {
+    manifest: {
+      name: 'MatchMaker',
+      //TBC.
+      short_name: 'MM',
+      lang: 'en',
+      description: 'An application that allows studnets to find their perfect supervisor match',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icons/MM_512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/MM_192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+      ],
+      screenshots: [
+        {
+          src: '/screenshots/Login.png',
+          sizes: '320x657',
+          type: 'image/png',
+          form_factor: 'wide'
+        },
+        {
+          src: '/screenshots/Chat.png',
+          sizes: '320x657',
+          type: 'image/png',
+        },
+        {
+          src: '/screenshots/Dashboard.png',
+          sizes: '320x657',
+          type: 'image/png',
+        },
+        {
+          src: '/screenshots/Search.png',
+          sizes: '320x657',
+          type: 'image/png',
+        },
+      ]
+    },
+
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico,woff2}'], 
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    }
+  },
+
 })

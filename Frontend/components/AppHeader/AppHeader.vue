91a161a1f8c7b66473ee2710a2db534372715d4c
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-04-22 16:08:54 +0200

    SCRUM 74: Nuxt FE: ensure proper bubbling of search field to calling code + input field appears below the header

--- Frontend/components/AppHeader/AppHeader.vue
+++ Frontend/components/AppHeader/AppHeader.vue
@@ -16,68 +16,82 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  modelValue: {
+    type: String,
+    default: '',
+  },
 })
 
 const isSearching = ref(false)
-const searchQuery = ref('')
 
-watch(searchQuery, (val) => {
-  console.log('Search query:', val)
-})
+const emit = defineEmits(['update:modelValue'])
 
-function goBack() {
-  router.back()
+function handleInput(value) {
+  emit('update:modelValue', value)
 }
+
 </script>
 
 <template>
-    <div class="navbar bg-base-100 shadow-sm">
-      <!-- Left: back or user -->
-        <div class="navbar-start">
-          <button
-              v-if="showBack"
-              class="btn btn-ghost btn-circle"
-              @click="goBack"
-              aria-label="Go back"
-          >
-            <FontAwesomeIcon icon="arrow-left" class="text-xl" />
-          </button>
+  <div class="w-full">
+    <div class="navbar bg-base-100 shadow z-10">
+      <div class="navbar-start ps-3">
+        <button
+          v-if="showBack"
+          class="btn btn-ghost btn-circle"
+          @click="goBack"
+        >
+          <FontAwesomeIcon icon="arrow-left" class="text-xl" />
+          <span class="text-sm font-medium opacity-50	">Back</span>
+        </button>
 
-          <button
-            v-if="showUser"
-            class="btn btn-ghost btn-circle"
-            aria-label="User"
-          >
-            <FontAwesomeIcon icon="user" class="text-xl" />
-          </button>
-        </div>
-  
-        <!-- Center: logo -->
-        <div class="navbar-center">
-          <img src="" alt="Logo" class="h-6" />
-        </div>
-  
-        <!-- Right: search -->
-        <div class="navbar-end">
-          <div v-if="showSearch">
-            <button
-              v-if="!isSearching"
-              class="btn btn-ghost btn-circle"
-              @click="isSearching = true"
-              aria-label="Open search"
-            >
-              <FontAwesomeIcon icon="search" class="text-xl" />
-            </button>
+        <button
+          v-if="showUser"
+          class="btn btn-ghost btn-circle"
+        >
+          <FontAwesomeIcon icon="user" class="text-xl" />
+        </button>
+      </div>
 
-            <div v-else class="w-48">
-              <InputField
-                v-model="searchQuery"
-                label=""
-                note=""
-                @blur="isSearching = false"
-              />
-            </div>
-          </div>
-        </div>
+      <div class="navbar-center">
+        <img src="/" alt="Logo" class="h-6" />
+      </div>
+
+      <div class="navbar-end">
+        <button
+          v-if="showSearch"
+          class="btn btn-ghost btn-circle"
+          @click="isSearching = !isSearching"
+        >
+          <FontAwesomeIcon icon="search" class="text-xl" />
+        </button>
+      </div>
     </div>
-  </template>
+
+    <transition name="fade">
+      <div
+        v-if="isSearching"
+        class="bg-base-100 px-4 pt-3 pb-4 border-t border-base-300"
+      >
+        <InputField
+          :modelValue="modelValue"
+          @update:modelValue="handleInput"
+          label="Seach Field"
+          note=""
+          @blur="isSearching = false"
+        />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>

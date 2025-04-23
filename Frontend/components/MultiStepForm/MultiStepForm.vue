274a9483795678e684a91d2fa4ccea9a1d2f588b
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-04-23 19:22:16 +0200

    SCRUM-77: Nuxt FE: create form-builder component

---
+++ Frontend/components/MultiStepForm/MultiStepForm.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  totalSteps: {
+    type: Number,
+    required: true
+  }
+})
+
+const emit = defineEmits(['submit'])
+
+const currentStep = ref(1)
+
+function next() {
+  if (currentStep.value < props.totalSteps) currentStep.value++
+}
+
+function back() {
+  if (currentStep.value > 1) currentStep.value--
+}
+
+function submit() {
+  emit('submit')
+}
+</script>
+
+<template>
+  <form @submit.prevent="submit" class="space-y-6 max-w-md mx-auto p-4">
+    <!-- Dynamic slots for each step -->
+    <slot :name="`step${currentStep}`" />
+
+    <!-- TODO: Replace buttons with custom component button as soon as it is ready -->
+    <div class="flex justify-center pt-4">
+        <button
+            v-if="currentStep < totalSteps"
+            type="button"
+            class="btn w-3/4"
+            @click="next"
+            data-test="next-button"
+        >
+            next
+        </button>
+
+        <button
+            v-else
+            type="submit"
+            class="btn w-3/4"
+            data-test="submit-button"
+        >
+            Submits
+        </button>
+    </div>
+  </form>
+</template>

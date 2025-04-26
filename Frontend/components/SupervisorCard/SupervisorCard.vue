430476a27a9b36222a5c5bdb7bb06af17bfa07b2
jb-cc <115902941+jb-cc@users.noreply.github.com>
2025-04-27 01:24:56 +0200

    feat(frontend): start supervisor card component (SCRUM-84)

---
+++ Frontend/components/SupervisorCard/SupervisorCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps({
+    image: {
+        type: String,
+        default: '',
+    },
+    name: {
+        type: String,
+        default: '',
+    },
+    tags: {
+        type: Array,
+        default: () => [],
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    similarityScore: {
+        type: Number,
+        default: 0,
+    },
+    maxCapacity: {
+        type: Number,
+        default: 0,
+    },
+    currentCapacity: {
+        type: Number,
+        default: 0,
+    },
+    pendingAmount: {
+        type: Number,
+        default: 0,
+    },
+    dragging: {
+        type: Boolean,
+        default: false,
+    },
+    iconSwipeLeft: {
+        type: String,
+        default: 'trash',
+    },
+    iconSwipeRight: {
+        type: String,
+        default: 'check',
+    },
+})
+
+const emit = defineEmits(['swipeLeft', 'swipeRight'])
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

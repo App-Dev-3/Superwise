e7c38e1f7b943078a6278b37282a64799a7e008a
jb-cc <115902941+jb-cc@users.noreply.github.com>
2025-04-27 14:51:29 +0200

    fix(frontend): fix cards not stacking on top of each other (SCRUM-84)

--- Frontend/components/CardStack/CardStack.vue
+++ Frontend/components/CardStack/CardStack.vue
@@ -5,21 +5,31 @@ const props = defineProps({
         default: 3,
     },
 })
+
+function getRotationStyle(index) {
+    if (index === 1) {
+        return 'rotate(0deg)';
+    }
+    return index % 2 === 0 ? 'rotate(-3deg)' : 'rotate(3deg)';
+}
 </script>
 
 <template>
-    <div class="stack w-96">
-        <template v-for="index in props.amount" :key="index">
-            <div :style="{ transform: `rotate(${index === 1 ? 0 : (index % 2 === 0 ? -3 : 3)}deg)` }">
-                <slot>
-                    <div
-                        class="h-48 w-48 bg-error text-error-content grid place-content-center rounded-box text-center">
-                        You forgot to add a Card in the "CardStack" Component! <br>
-                        Go buy drinks🍻<br>
-                        <p class="text-xs">(the tag should also not be self-closing)</p>
-                    </div>
-                </slot>
-            </div>
-        </template>
+    <div class="stack relative w-fit h-fit">
+        <div
+            v-for="index in props.amount"
+            :key="index"
+            :style="{ transform: getRotationStyle(index) }"
+            class="absolute"
+        >
+            <slot>
+                <div
+                    class="h-48 w-48 bg-error text-error-content grid place-content-center rounded-box text-center">
+                    You forgot to add a Card in the "CardStack" Component! <br>
+                    Go buy drinks🍻<br>
+                    <p class="text-xs">(the tag should also not be self-closing)</p>
+                </div>
+            </slot>
+        </div>
     </div>
 </template>

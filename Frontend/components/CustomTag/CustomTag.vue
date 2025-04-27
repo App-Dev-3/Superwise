221e5a9b22204c9ef429631add676b47a3e8cdcb
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-04-27 23:25:21 +0200

    Squashed commit of the following:
    commit 31f81c1e229a1f3560b19c4e1d5677de1f45fa76
    Author: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    Date:   Sun Apr 27 23:14:45 2025 +0200
    
        SCRUM-79: Nuxt FE: update custom tag unit tests
    
    commit 081dc0f52bf6d780b7b63364c9d2eed1a8fae03d
    Author: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    Date:   Sun Apr 27 23:08:42 2025 +0200
    
        SCRUM-79: Nuxt FE: Allow whole tag to be clickable, while conditionally showing the x mark for deleting
    
    commit 59f7f9ea77fe253d8ab537d758fa55746b0c6c8a
    Author: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    Date:   Sun Apr 27 22:51:57 2025 +0200
    
        SCRUM-79: Nuxt FE: add unit tests
    
    commit e8b977224127a4a1af25b45070c355df15635ce8
    Author: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    Date:   Sun Apr 27 22:39:31 2025 +0200
    
        Squashed commit of the following:
    
        commit 72cefdedcaa482f15a720afe7739fbce19dfa28c
        Author: Jonas Beer <115902941+jb-cc@users.noreply.github.com>
        Date:   Sun Apr 27 01:30:52 2025 +0200
    
            chore(frontend): update dependencies to fix a moderate vulnerability (#25)
    
        commit f06d677b72baf5762904685b1abfbac3a6646edc
        Author: LionRise <111492453+LionRise@users.noreply.github.com>
        Date:   Fri Apr 25 20:50:56 2025 +0200
    
            Scrum 80 nuxt fe nuxt be implement testing (#22)
    
            Co-authored-by: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    
        commit 91eb464c5b33d14f743ce5479b1659af21e69ee5
        Author: Hesham Alhuraibi <46753692+nb159@users.noreply.github.com>
        Date:   Fri Apr 25 15:27:25 2025 +0200
    
            Scrum-xx: Nuxt FE: apply fixes to custom component (#23)
    
    commit ef79f434c0dc2585e8624c8cd7542bdea79b65ad
    Author: Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
    Date:   Sun Apr 27 22:39:15 2025 +0200
    
        SCRUM-79: Nuxt FE: Create tag component

---
+++ Frontend/components/CustomTag/CustomTag.vue
@@ -0,0 +1,55 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Tag',
+  },
+  color: {
+    type: String,
+    default: 'primary',
+  },
+  clickable: {
+    type: Boolean,
+    default: false,
+  },
+  deletable: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['click', 'delete'])
+
+function handleClick() {
+  if (props.clickable && !props.deletable) {
+    emit('click')
+  }
+}
+
+function handleDelete() {
+  if (props.deletable) {
+    emit('delete')
+  }
+}
+</script>
+
+<template>
+  <div 
+    class="badge badge-neutral badge-outline p-1"
+    :class="[
+      `bg-${color}`, 
+      `text-${color}-content`, 
+    ]"
+    @click="handleClick"
+  >
+    <FontAwesomeIcon
+      v-if="deletable"
+      icon="xmark"
+      class="text-s cursor-pointer"
+      data-test="icon"
+      @click="handleDelete"
+    />
+      {{ name }}
+  </div>
+
+</template>

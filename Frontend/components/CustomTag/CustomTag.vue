f52455557a6a865b67357b7d15db5ee2bf9d199e
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-04-27 22:39:15 +0200

    SCRUM-79: Nuxt FE: Create tag component

---
+++ Frontend/components/CustomTag/CustomTag.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Tag',
+  },
+  color: {
+    type: String,
+    default: 'primary',
+  },
+  clickable: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['click'])
+
+function handleClick() {
+  if (clickable) {
+    emit('click')
+  }
+}
+</script>
+
+<template>
+  <div 
+    class="badge badge-neutral badge-outline p-1"
+    :class="[
+      `bg-${color}`, 
+      `text-${color}-content`, 
+    ]"
+  >
+    <FontAwesomeIcon
+      v-if="clickable"
+      icon="xmark"
+      class="text-s cursor-pointer"
+      @click="handleClick"
+    />
+      {{ name }}
+  </div>
+
+</template>

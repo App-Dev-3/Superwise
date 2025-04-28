5b8ddd21d13c75c25abaaf7d8399a39605f4702e
LionRise <lenastadlinger@gmail.com>
2025-04-28 13:21:20 +0200

    added auth and onboarding middleware

---
+++ Frontend/pages/onboarding/student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+      <SignedIn>
+        <div v-if="!isLoaded">Loading...</div>
+
+        <div v-else>Hello {{ user?.primaryEmailAddress }}!</div>
+      </SignedIn>
+    </header>
+  </div>
+</template>
+
+<script setup>
+import { useUser } from "@clerk/vue";
+
+definePageMeta({
+  layout: "authenticated",
+});
+
+const { user, isLoaded } = useUser();
+</script>

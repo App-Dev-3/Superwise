7ca8317f103476913f39f87bdc8eefa66555da5f
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2025-04-28 18:59:18 +0200

    feat(frontend): Add customtag to supervisor card (SCRUM-84)

--- Frontend/components/SupervisorCard/SupervisorCard.test.js
+++ Frontend/components/SupervisorCard/SupervisorCard.test.js
@@ -1,7 +1,12 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect, vi } from 'vitest'
-import { nextTick } from 'vue'
 import SupervisorCard from './SupervisorCard.vue'
+import CustomTag from '../CustomTag/CustomTag.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserGroup, faHourglass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserGroup, faHourglass)
 
 describe('SupervisorCard', () => {
     const defaultProps = {
@@ -13,17 +18,36 @@ describe('SupervisorCard', () => {
         currentCapacity: 50,
     }
 
+    const globalComponents = {
+        CustomTag,
+        FontAwesomeIcon
+    }
+
     it('renders correctly with required props', () => {
         const wrapper = mount(SupervisorCard, {
-            props: defaultProps
+            props: defaultProps,
+            global: {
+                components: globalComponents
+            }
         })
 
         expect(wrapper.find('img').attributes('src')).toBe(defaultProps.image)
         expect(wrapper.text()).toContain(defaultProps.name)
-        expect(wrapper.text()).toContain('75 %') // similarity score
-        expect(wrapper.text()).toContain('50/100') // capacity
-    })
 
+        // Check tags
+        const tags = wrapper.findAllComponents(CustomTag)
+        expect(tags).toHaveLength(6) // 3 regular tags + similarity score + capacity + pending
+
+        // Check scores and capacity
+        const scoreTag = wrapper.findAllComponents(CustomTag)
+            .find(tag => tag.props('text') === '75%')
+        expect(scoreTag).toBeTruthy()
+
+        const capacityTag = wrapper.findAllComponents(CustomTag)
+            .find(tag => tag.props('text') === '50/100 ')
+        expect(capacityTag).toBeTruthy()
+        expect(capacityTag.props('rightIcon')).toBe('user-group')
+    })
 
     it('displays optional description when provided', () => {
         const description = 'This is a test description'
@@ -31,6 +55,9 @@ describe('SupervisorCard', () => {
             props: {
                 ...defaultProps,
                 description
+            },
+            global: {
+                components: globalComponents
             }
         })
 
@@ -42,9 +69,32 @@ describe('SupervisorCard', () => {
             props: {
                 ...defaultProps,
                 pendingAmount: 5
+            },
+            global: {
+                components: globalComponents
             }
         })
 
-        expect(wrapper.text()).toContain('5')
+        const pendingTag = wrapper.findAllComponents(CustomTag)
+            .find(tag => tag.props('text') === '5 ')
+        expect(pendingTag).toBeTruthy()
+        expect(pendingTag.props('rightIcon')).toBe('hourglass')
+    })
+
+    it('limits the number of displayed tags according to maxTagAmount', () => {
+        const wrapper = mount(SupervisorCard, {
+            props: {
+                ...defaultProps,
+                tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'],
+                maxTagAmount: 3
+            },
+            global: {
+                components: globalComponents
+            }
+        })
+
+        const regularTags = wrapper.findAllComponents(CustomTag)
+            .filter(tag => !tag.props('rightIcon') && !tag.props('text').includes('%'))
+        expect(regularTags).toHaveLength(3)
     })
 })
--- Frontend/plugins/fontawesome.js
+++ Frontend/plugins/fontawesome.js
@@ -1,16 +1,17 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
   faArrowLeft,
   faUser,
   faSearch,
   faEnvelope,
   faXmark,
-} from "@fortawesome/free-solid-svg-icons";
-import { defineNuxtPlugin } from "nuxt/app";
+  faHourglass,
+  faUserGroup
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faArrowLeft, faUser, faSearch, faEnvelope, faXmark);
+library.add(faArrowLeft, faUser, faSearch, faEnvelope, faXmark, faHourglass, faUserGroup)
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component("FontAwesomeIcon", FontAwesomeIcon);
-});
+  nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
+})

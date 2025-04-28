156c66cca0a76df9601279ee36b53d1928c21788
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-04-29 01:03:41 +0200

    Squashed commit of the following:
    commit 697c43982174872152ca0ec24a62d4b19b978868
    Author: Hesham Alhuraibi <46753692+nb159@users.noreply.github.com>
    Date:   Mon Apr 28 18:27:12 2025 +0200
    
        Scrum-79: Nuxt FE: create tags custom component (#28)
    
        Co-authored-by: Jonas Beer <115902941+jb-cc@users.noreply.github.com>

--- Frontend/components/CustomTag/CustomTag.vue
+++ Frontend/components/CustomTag/CustomTag.vue
@@ -1,21 +1,48 @@
 <script setup>
+import { computed } from "vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 const props = defineProps({
-  name: {
-    type: String,
-    default: 'Tag',
-  },
-  color: {
-    type: String,
-    default: 'primary',
-  },
-  clickable: {
-    type: Boolean,
-    default: false,
-  },
-  deletable: {
-    type: Boolean,
-    default: false,
-  },
+    text: { type: String, default: 'Tag' },
+    color: {
+        type: String,
+        default: 'primary',
+        validator(value, props) {
+            // TLDR: The color of a badge is constrained to daisyUI colors if the variant is not 'clear' or 'ghost'.
+            // If the variant is 'clear' or 'ghost', the color can be any color.
+            const currentVariant = props.variant || 'default'
+            const daisyColors = ['primary', 'secondary', 'accent', 'neutral', 'info', 'success', 'warning', 'error']
+            const clearVariants = ['clear', 'ghost']
+            if (clearVariants.includes(currentVariant)) {
+                return true
+            } else {
+                const isValidColor = daisyColors.includes(value)
+                if (!isValidColor) {
+                    console.warn(`Invalid color "${value}" for variant "${currentVariant}". Valid colors are: ${daisyColors.join(', ')}`)
+                }
+                return isValidColor
+            }
+        }
+    },
+    variant: {
+        type: String,
+        default: 'default',
+        validator(value, props) {
+            // FYI: the 'clear' variant is custom. It removes all the background and border styles.
+            // (the 'ghost' variant from daisy was not enough to do that)
+            return ['default', 'outline', 'ghost', 'soft', 'dash', 'clear'].includes(value);
+        }
+    },
+    size: {
+        type: String,
+        default: 'md',
+        validator(value, props) {
+            return ['xs', 'sm', 'md', 'lg', 'xl'].includes(value)
+        }
+    },
+    rightIcon: { type: String, default: '' },
+    clickable: { type: Boolean, default: false },
+    deletable: { type: Boolean, default: false },
 })
 
 const emit = defineEmits(['click', 'delete'])
@@ -31,25 +58,84 @@ function handleDelete() {
     emit('delete')
   }
 }
+
+const badgeClasses = computed(() => {
+  // i know we hate margins but with paddings it doesnt work, because padding just pushes the border away
+    const classes = ['badge', 'm-0.5']
+
+    // Color classes
+    const daisyColors = ['primary', 'secondary', 'accent', 'neutral', 'info', 'success', 'warning', 'error']
+    const invisibleVariants = ['clear', 'ghost']
+    if (!invisibleVariants.includes(props.variant)) { // if not a clear or ghost variant
+        if (daisyColors.includes(props.color)) { // if the color is a daisy color, create normal badge
+            classes.push(`badge-${props.color}`)
+        }
+    } else {
+        classes.push(`text-${props.color}`)
+    }
+
+
+    // Variant classes
+    switch (props.variant) {
+        case 'outline':
+            classes.push('badge-outline')
+            break
+        case 'ghost':
+            classes.push('badge-ghost')
+            break
+        case 'soft':
+            classes.push('badge-soft')
+            break
+        case 'dash':
+            classes.push('badge-dash')
+            break
+        case 'clear':
+            classes.push('bg-transparent')
+            classes.push('border-none')
+            break
+        default:
+            break
+    }
+
+    // Size classes
+    classes.push(`badge-${props.size}`)
+
+    // Clickable styling
+    if (props.clickable) {
+        classes.push('cursor-pointer')
+    }
+
+    return classes
+})
+
+const iconColorClass = computed(() => {
+    return `!fill-${props.color}`
+})
+
 </script>
 
 <template>
-  <div 
-    class="badge badge-neutral badge-outline p-1"
-    :class="[
-      `bg-${color}`, 
-      `text-${color}-content`, 
-    ]"
-    @click="handleClick"
-  >
-    <FontAwesomeIcon
-      v-if="deletable"
-      icon="xmark"
-      class="text-s cursor-pointer"
-      data-test="icon"
-      @click="handleDelete"
-    />
-      {{ name }}
-  </div>
+    <div
+        :class="badgeClasses"
+        @click="handleClick"
+    >
+        <FontAwesomeIcon
+            v-if="props.deletable"
+            icon="xmark"
+            class="text-s cursor-pointer"
+            :class="iconColorClass"
+            data-test="delete-icon"
+            @click="handleDelete"
+        />
+        <span >
+            {{ props.text }}
+            <FontAwesomeIcon
+                v-if="props.rightIcon"
+                :icon="props.rightIcon"
+                :class="iconColorClass"
+                data-test="prop-icon"
+            />
+        </span>
+    </div>
 
 </template>

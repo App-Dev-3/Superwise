9371be44e76fcf76740b8e962d78885f481f4af7
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2025-04-30 10:36:30 +0200

    Revert "Scrum 86 nest move all database interaction from services" (#38)

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
-  exports: [UsersService, UsersRepository],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}

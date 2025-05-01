85b4f9df4055e68fefbad9b1f9c7c8e95ee3ea24
LionRise <lenastadlinger@gmail.com>
2025-05-01 16:51:09 +0200

    major pwa update, added typescript as a dependency, added layouts and clerk onboarding and consisten auth

--- Frontend/pages/onboarding/student.vue
+++ Frontend/pages/onboarding/student.vue
@@ -1,24 +1,42 @@
 <template>
-  <div>
-    <header>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-      <SignedIn>
-        <div v-if="!isLoaded">Loading...</div>
+  <SignedOut>
+    <RedirectToSignIn />
+  </SignedOut>
+  <SignedIn>
+    <div class="flex flex-col items-center justify-center h-screen p-4">
+      <h1 class="text-2xl font-semibold mb-6">Welcome to student onboarding</h1>
+      <span v-if="!isLoaded">Loading...</span>
 
-        <div v-else>Hello {{ user?.primaryEmailAddress }}!</div>
-      </SignedIn>
-    </header>
-  </div>
+      <button
+        v-else
+        class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
+        :disabled="!isLoaded"
+        @click="finishOnboarding"
+      >
+        <span>Finish onboarding</span>
+      </button>
+    </div>
+  </SignedIn>
 </template>
 
 <script setup>
 import { useUser } from "@clerk/vue";
 
 definePageMeta({
-  layout: "authenticated",
+  layout: "default",
 });
 
-const { user, isLoaded } = useUser();
+const { isLoaded, isSignedIn, user } = useUser();
+
+const finishOnboarding = async () => {
+  if (!isLoaded.value || !isSignedIn.value || !user.value) return;
+
+  try {
+    await user.value.update({ unsafeMetadata: { onboardingCompleted: true } });
+
+    return navigateTo(`/dashboard`);
+  } catch (err) {
+    console.error("Onboarding error:", err);
+  }
+};
 </script>

8a2569ccb39f0e8f473c12b73cc0883aa04749b1
LionRise <lenastadlinger@gmail.com>
2025-05-02 13:09:13 +0200

    feat(frontend): clean install and fixed lint errors (SCRUM-37)

--- Frontend/pages/onboarding/student.vue
+++ Frontend/pages/onboarding/student.vue
@@ -1,38 +1,26 @@
 <template>
-  <<<<<<< HEAD
-  <SignedOut>
-    <RedirectToSignIn />
-  </SignedOut>
-  <SignedIn>
-    <div class="flex flex-col items-center justify-center h-screen p-4">
-      <h1 class="text-2xl font-semibold mb-6">Welcome to student onboarding</h1>
-      <span v-if="!isLoaded">Loading...</span>
+  <div>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+    <SignedIn>
+      <div class="flex flex-col items-center justify-center h-screen p-4">
+        <h1 class="text-2xl font-semibold mb-6">
+          Welcome to student onboarding
+        </h1>
+        <span v-if="!isLoaded">Loading...</span>
 
-      <button
-        v-else
-        class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
-        :disabled="!isLoaded"
-        @click="finishOnboarding"
-      >
-        <span>Finish onboarding</span>
-      </button>
-    </div>
-  </SignedIn>
-  =======
-  <div class="flex flex-col items-center justify-center h-screen p-4">
-    <h1 class="text-2xl font-semibold mb-6">Welcome to Onboarding</h1>
-    <span v-if="!isLoaded">Loading...</span>
-
-    <button
-      v-else
-      class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
-      :disabled="!isLoaded"
-      @click="finishOnboarding"
-    >
-      <span>Finish onboarding</span>
-    </button>
+        <button
+          v-else
+          class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          :disabled="!isLoaded"
+          @click="finishOnboarding"
+        >
+          <span>Finish onboarding</span>
+        </button>
+      </div>
+    </SignedIn>
   </div>
-  >>>>>>> 439f63f2042719521c9c050e8eb3223cb104b1a2
 </template>
 
 <script setup lang="ts">

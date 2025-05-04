3dd1477936da57d70f6f38795936242b12153085
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2025-05-04 21:02:15 +0200

    feat(frontend): add StatusBar component (SCRUM-99) (#51)

---
+++ Frontend/components/StatusBar/StatusBar.vue
@@ -0,0 +1,46 @@
+<script setup>
+    import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+    const props = defineProps({
+        step: {
+            type: Number,
+            default: 1,
+        },
+        smallText: {
+            type: Boolean,
+            default: true,
+        },
+    })
+</script>
+
+<template>
+    <ul class="steps w-full">
+        <li
+            class="step text-base-content font-bold"
+            :class="{'step-primary': props.step >= 1, 'text-sm': props.smallText}"
+        >
+            <span class="step-icon" >
+                <FontAwesomeIcon icon="arrow-right-to-bracket"/>
+            </span>
+            Onboarding
+        </li>
+        <li
+            class="step text-base-content font-bold"
+            :class="{'step-primary': props.step >= 2, 'text-sm': props.smallText}"
+        >
+            <span class="step-icon" >
+                <FontAwesomeIcon icon="message"/>
+            </span>
+            Matching
+        </li>
+        <li
+            class="step text-base-content font-bold"
+            :class="{'step-primary': props.step === 3, 'text-sm': props.smallText}"
+        >
+            <span class="step-icon" >
+                <FontAwesomeIcon icon="user-group"/>
+            </span>
+            Confirmed
+        </li>
+    </ul>
+</template>
--- Frontend/plugins/fontawesome.js
+++ Frontend/plugins/fontawesome.js
@@ -10,10 +10,28 @@ import {
   faUserGroup,
   faMoon,
   faSun,
+  faHandshakeSimple,
+  faMessage,
+  faArrowRightToBracket,
+  faCheck
 } from '@fortawesome/free-solid-svg-icons'
 import {defineNuxtPlugin} from "nuxt/app";
 
-library.add(faArrowLeft, faUser, faSearch, faEnvelope, faXmark, faHourglass, faUserGroup, faMoon, faSun)
+library.add(
+    faArrowLeft,
+    faUser,
+    faSearch,
+    faEnvelope,
+    faXmark,
+    faHourglass,
+    faUserGroup,
+    faArrowRightToBracket,
+    faMessage,
+    faHandshakeSimple,
+    faCheck,
+    faMoon,
+    faSun
+)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)

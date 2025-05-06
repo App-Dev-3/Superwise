8abaecaae8d46dd5f5d4c819911d7f318bb71d20
LionRise <lenastadlinger@gmail.com>
2025-05-06 13:57:38 +0200

    fixed duplication in default layout

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -15,21 +15,3 @@
     </SignedIn>
   </div>
 </template>
-
-<template>
-  <div>
-    <SignedOut>
-      <div>
-        <SignInButton />
-        <slot />
-      </div>
-    </SignedOut>
-
-    <SignedIn>
-      <div>
-        <UserButton />
-        <slot />
-      </div>
-    </SignedIn>
-  </div>
-</template>

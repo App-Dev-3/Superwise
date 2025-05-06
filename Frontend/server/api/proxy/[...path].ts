ce75e6cd1fea233a016c067eeea2087035bec35f
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-05-07 01:43:04 +0200

    SCRUM: Nuxt FE(Feature): Make proxy reactive to request method

--- Frontend/server/api/proxy/[...path].ts
+++ Frontend/server/api/proxy/[...path].ts
@@ -1,27 +1,48 @@
 import { joinURL } from 'ufo'
+import { getAuth } from '@clerk/nuxt/server'
+import { url } from 'inspector'
+import { json } from 'stream/consumers'
+import { FormData } from 'happy-dom'
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const path = getRouterParam(event, 'path') ?? '' 
-  const target = joinURL(config.nestApiUrl, path)
+  let target = joinURL(config.nestApiUrl, path)
   
+  const incomingBody = await readBody(event)
+  const method = incomingBody.action || 'GET'
+  const data = incomingBody.data || {}
+
+  //clerk token
+  const { getToken } = getAuth(event)
+  const token = await getToken()
+
   const headers = {
     'Content-Type': 'application/json',
-    'bearer-token': 'bebebobo',
+    'bearer-token': token,
   }
-  
-  let body: any = null
-  const method = event.node.req.method ?? 'GET'
-  if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
-    body = await readBody(event)
+
+  let body: any = undefined
+  if (method === 'GET' && data) {
+    //append search param to url since GET shouldnt have a body
+    const url = new URL(target)
+    Object.entries(data).forEach(([key, value]) => {
+      if (value !== null && value !== undefined) {
+        if (key === 'empty') key =''
+        url.searchParams.append(key, String(value))
+      }
+    })
+
+    target = url.toString()
+  } else if (data){ 
+    body = JSON.stringify(data)
   }
 
   const response = await fetch(target, {
     method,
     headers: headers,
-    body: body ? JSON.stringify(body) : undefined,
+    body
   })
 
-  const data = await response.text()
-  return data  
+  return response.text()  
 })

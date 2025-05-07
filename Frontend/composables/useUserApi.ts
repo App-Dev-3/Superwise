b750913e590584234fbc8990f5d79f2f67f07c0a
Hesham Alhuraibi <46753692+nb159@users.noreply.github.com>
2025-05-07 20:50:37 +0200

    feat(frontend): secure communication with nest js backend, add api call wrapper (Scrum-41) (#55)
    Co-authored-by: jb-cc <115902941+jb-cc@users.noreply.github.com>

---
+++ Frontend/composables/useUserApi.ts
@@ -0,0 +1,38 @@
+import { makeRequest } from './useApi'
+import type {UserCreateData} from "~/shared/types/userInterfaces";
+
+export const useUserApi = () => {
+  const getUserRole = async (data: {email:string}) => {
+    console.log('getUserRole', data)
+    return await makeRequest('user/role', 'POST', data) 
+  }
+
+  const createUser = async (data: UserCreateData) => {
+    return await makeRequest('users', 'POST', data)
+  }
+
+  const getUserByEmail = async (data: string) => {
+    return await makeRequest('users/search/by-email', 'GET', {
+      email: data
+    })
+  }
+
+  const getUserById = async (data: string) => {
+    return await makeRequest(`users/${data}`, 'GET')
+  }
+  
+  //TODO: upload tags to use this endpoint
+  const addUserTag = async (data: Record<string, unknown>) => {
+    console.log('addUserTag', data)
+    return await makeRequest(`users/${data.id}/tags`, 'PUT', {tags: data.tags})
+  }
+
+  return {
+    getUserRole,
+    createUser,
+    getUserByEmail,
+    getUserById,
+    addUserTag
+  }
+
+}

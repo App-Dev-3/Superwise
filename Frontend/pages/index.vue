2972c681b04670d62d8abf8458c82de21e724704
FelixTeutsch <felix@teutsch.it>
2025-05-09 01:21:19 +0200

    feat(frontend): improve landing page layout and add navigation to onboarding

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,40 +1,54 @@
 <template>
+  <div class="w-full h-screen flex flex-col justify-center items-center p-8 gap-4">
+    <ClientOnly>
+      <img
+          :src="colorMode.value === 'dark' ? 'images/appLanding_logo_dark.svg' : 'images/appLanding_logo_light.svg'"
+          alt="Logo image"
+          class="w-full max-w-96">
+    </ClientOnly>
 
-  <div class="w-full h-screen flex flex-col justify-center items-center">
-    <div class="w-full h-full flex flex-col p-8 gap-4">
-      <div class="flex justify-center w-full p-y">
-        <ClientOnly>
-          <img
-              :src="colorMode.value === 'dark' ? 'images/appLanding_logo_dark.svg' : 'images/appLanding_logo_light.svg'"
-              alt="Logo image"
-              class="w-full max-w-96">
-        </ClientOnly>
+    <div class="py-16 flex flex-col gap-4 h-full">
+      <div class="text-x-small">
+        In order to find the perfect supervisor for you, we have to collect and process some data.
       </div>
-      <div class="py-16 flex flex-col gap-4">
-        <div class="text-x-small">
-          In order to find the perfect supervisor for you, we hace to collect and process some data.
-        </div>
-        <div class="text-x-small">
-          <!-- TODO: make a link to actual terms and condition -->
-          Find out <a class="text-xs-link" href="https://youtu.be/dQw4w9WgXcQ?feature=shared">here</a> what we do with
-          it!
-        </div>
+      <div class="text-x-small">
+        <!-- TODO: make a link to actual terms and condition -->
+        Find out <a class="text-xs-link" href="https://youtu.be/dQw4w9WgXcQ?feature=shared">here</a> what we do with
+        it!
       </div>
     </div>
 
-    <div class="w-full p-4 justify-center">
-      <CustomButton
-          block
-          color="primary"
-          right-icon="arrow-right"
-          text="Continue"
-      />
-    </div>
+    <CustomButton
+        block
+        class="w-full"
+        color="primary"
+        right-icon="arrow-right"
+        text="Continue"
+        @click="navigateToNext"
+    />
   </div>
 </template>
 
 <script setup>
 import {useColorMode} from "#imports";
+import {useRouter} from 'vue-router';
 
 const colorMode = useColorMode();
+const router = useRouter();
+
+const navigateToNext = () => {
+  router.push('/onboarding');
+};
 </script>
+
+<style scoped>
+.text-x-small {
+  font-size: 0.875rem;
+  color: var(--color-base-content);
+}
+
+.text-xs-link, a {
+  color: var(--color-primary);
+  text-decoration: underline;
+}
+</style>

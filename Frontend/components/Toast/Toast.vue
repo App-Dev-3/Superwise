7292007cdc24a210716a12ed9f90ae9722ece289
FelixTeutsch <felix@teutsch.it>
2025-05-08 21:58:55 +0200

    fix(frontend): add onBeforeUnmount lifecycle hook to Toast component

--- Frontend/components/Toast/Toast.vue
+++ Frontend/components/Toast/Toast.vue
@@ -22,7 +22,7 @@ const props = defineProps({
     }
 });
 
-const emit = defineEmits(['buttonClick']);
+const emit = defineEmits(['buttonClick', 'close']);
 
 // Internal visibility state
 const visible = ref(true);

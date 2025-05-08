1c165f4b9c480db3c831d12e1abd63f39616991d
FelixTeutsch <felix@teutsch.it>
2025-05-08 17:23:42 +0200

    feat(MiniCard): refactor layout and styling for improved avatar display and text alignment

--- Frontend/components/MiniCard/MiniCard.vue
+++ Frontend/components/MiniCard/MiniCard.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { computed } from "vue";
 import CustomTag from "../CustomTag/CustomTag.vue";
 
 const props = defineProps({
-  size: {
-    type: String,
-    default: "md",
-    required: false,
-    validator: (value) => ["xs", "sm", "md", "lg", "xl"].includes(value),
-  },
   fullWidth: {
     type: Boolean,
     default: false,
@@ -52,62 +45,49 @@ const props = defineProps({
   },
 });
 
-const cardSizeClasses = computed(() => ({
-  "w-full sm:w-[32rem]": props.fullWidth,
-  "w-72 card-xs": !props.fullWidth && props.size === "xs",
-  "w-80 card-sm": !props.fullWidth && props.size === "sm",
-  "w-96 card-md": !props.fullWidth && props.size === "md",
-  "w-[28rem] card-lg": !props.fullWidth && props.size === "lg",
-  "w-[32rem] card-xl": !props.fullWidth && props.size === "xl",
-  "card-xs": props.fullWidth && props.size === "xs",
-  "card-sm": props.fullWidth && props.size === "sm",
-  "card-md": props.fullWidth && props.size === "md",
-  "card-lg": props.fullWidth && props.size === "lg",
-  "card-xl": props.fullWidth && props.size === "xl",
-}));
 </script>
 
 <template>
   <div
-    class="card card-side bg-base-100 p-2 w-100 h-auto gap-6 shadow-lg"
-    :class="cardSizeClasses"
+      class="flex flex-row gap-3 items-center p-2"
   >
-    <figure class="flex-shrink-0 w-24 md:w-32">
-      <img
-        class="rounded-box w-full h-auto object-cover"
-        :src="
-          props.image || getPlaceholderImage(props.firstName, props.lastName)
-        "
-        alt="Profile Picture of {{ props.firstName }} {{ props.lastName }}"
-      >
-    </figure>
-    <div class="card-body min-w-0">
-      <h2 class="card-title">
-        <p class="font-bold">{{ props.firstName }} {{ props.lastName }}</p>
-        <CustomTag
-          v-if="props.topText || props.topIcon"
-          :text="props.topText"
-          :right-icon="props.topIcon"
-          variant="clear"
-          color="base-content"
-          class="font-normal"
-          muted
-        />
-      </h2>
+    <div class="avatar">
+      <div class="mask mask-squircle w-24">
+        <img
+            :src="props.image || getPlaceholderImage(props.firstName, props.lastName)"
+            alt="Profile Picture of {{ props.firstName }} {{ props.lastName }}"
+        >
+      </div>
+    </div>
+    <div class="flex flex-col flex-auto min-w-0 h-fit">
+      <div class="flex justify-between">
+        <div class="text-large">{{ props.firstName }} {{ props.lastName }}</div>
+        <div class="opacity-75">
+          <CustomTag
+              v-if="props.topText || props.topIcon"
+              :right-icon="props.topIcon"
+              :text="props.topText"
+              class="font-normal"
+              color="base-content"
+              muted
+              variant="clear"
+          />
+        </div>
+      </div>
 
-      <div class="flex justify-between items-center gap-2">
-        <p class="text-base-content/75 truncate w-4/5">
+      <div class="flex justify-between items-center gap-2 opacity-50">
+        <div class="text-small text-base-content/75 truncate w-4/5">
           {{ props.previewText }}
-        </p>
+        </div>
         <div class="flex-shrink-0">
           <CustomTag
-            v-if="props.bottomText || props.bottomIcon"
-            :text="props.bottomText"
-            :right-icon="props.bottomIcon"
-            variant="clear"
-            color="base-content"
-            muted
-            class="text-sm"
+              v-if="props.bottomText || props.bottomIcon"
+              :right-icon="props.bottomIcon"
+              :text="props.bottomText"
+              class="text-sm"
+              color="base-content"
+              muted
+              variant="clear"
           />
         </div>
       </div>

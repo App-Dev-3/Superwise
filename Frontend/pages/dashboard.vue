61791cf61d9327b723d49563708008c2b27316e6
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2025-05-09 01:48:47 +0200

    feat(frontend): add Dashboard (SCRUM-45)

--- Frontend/pages/dashboard.vue
+++ Frontend/pages/dashboard.vue
@@ -1,13 +1,78 @@
 <template>
-  <div>
-    <header>
-      <div>Welcome to the dashboard</div>
-    </header>
-  </div>
+    <div class="flex flex-col items-center px-2 max-w-full">
+        <div class="min-h-screen flex flex-col max-w-7xl w-full">
+            <AppHeader show-search show-user />
+            <StatusBar :step="1" class="mt-4" />
+            <div class="my-auto mx-auto max-w-7xl w-full p-12">
+                <ActionCard button-text="Start Matching!" card-type="primary">
+                    <div class="flex flex-col w-full items-center p-3">
+                        <CardStack :amount="matches.length">
+                            <SupervisorCard
+                                size="lg"
+                                :tags="matches[0].tags"
+                                :current-capacity="matches[0].availableSpots"
+                                :max-capacity="matches[0].totalSpots"
+                                :similarity-score="matches[0].compatibilityScore"
+                                name="Hello name"
+                                image="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                :description="matches[0].bio"
+                            />
+                        </CardStack>
+                    </div>
+                </ActionCard>
+            </div>
+
+            <div class="max-w-7xl w-full">
+                <BottomNav
+                    :active-route="dummyRoute"
+                    :always-show-labels="false"
+                    @navigate="navigate"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from "vue";
+import BottomNav from "../components/BottomNav/BottomNav.vue";
+import AppHeader from "../components/AppHeader/AppHeader.vue";
+import StatusBar from "../components/StatusBar/StatusBar.vue";
+import ActionCard from "../components/ActionCard/ActionCard.vue";
+import { useUserStore } from "~/stores/useUserStore";
+import type { UserData } from "~/shared/types/userInterfaces";
+
 definePageMeta({
-  layout: "authenticated",
+    layout: "authenticated",
 });
+
+const { user } = useUser();
+const { getUserByEmail, getMatches} = useUserApi();
+const userStore = useUserStore();
+
+type Matches = Awaited<ReturnType<typeof getMatches>>;
+const matches = ref<Matches>([] as Matches);
+
+let current_user: UserData | null = userStore.user;
+
+if (!current_user && user.value?.primaryEmailAddress?.emailAddress) {
+    const res = (await getUserByEmail(
+        user.value?.primaryEmailAddress.emailAddress
+    )) as UserData;
+    userStore.setUser(res);
+
+    current_user = res;
+}
+
+if (current_user !== null) {
+    matches.value = await getMatches(current_user.id);
+    console.log(matches);
+}
+
+function navigate(route: string) {
+    dummyRoute.value = route;
+    console.log("Navigating to:", route);
+}
+
+const dummyRoute = ref("/");
 </script>

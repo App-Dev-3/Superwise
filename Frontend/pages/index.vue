8b947af7d199bd91380fa93b90178502eaca68dd
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-05-08 23:57:32 +0200

    Scrum-38: Nuxt FE (feature): Ensure index page acts as the first page accordingly

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <p>Please accept our terms and conditions</p>
+    <!-- Here goes the first page of the app. It should lead to clerk sign up then redirected to the dashboard
+    The dashboard is covered by an authentication layout which would redirect to onboarding
+    if user hasnt onboarded yet -->
 
+
+    <!-- This nuxt link should be removed once there is a page that the user can use to move on -->
     <NuxtLink to="/dashboard">Dashboard</NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
 import { NuxtLink } from "#components";
+const { isSignedIn } = useUser();
 
 definePageMeta({
-  layout: "authenticated",
+  layout: "default",
 });
 </script>

d72660b7aa9d9bd3db08e32a20f0b64b8cbaa6d3
FelixTeutsch <felix@teutsch.it>
2025-05-08 18:44:29 +0200

    feat(frontend): add Toast component without any logic

---
+++ Frontend/components/Toast/Toast.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
+
+<template>
+  <div
+      v-if="show"
+      class="fixed bottom-18 left-0 right-0 flex justify-center z-50 px-12"
+  >
+    <div class="alert alert-success w-full shadow-lg" role="alert">
+      <FontAwesomeIcon class="text-xl" icon="check"/>
+      <span>Your purchase has been confirmed!</span>
+      <CustomButton
+          color="default"
+          text="close"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

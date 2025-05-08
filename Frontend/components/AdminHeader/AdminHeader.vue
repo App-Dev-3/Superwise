a57acf3a86cfd0d9651e18f56bc04120d5a29179
FelixTeutsch <felix@teutsch.it>
2025-05-08 23:33:38 +0200

    feat(frontend): add AdminHeader component with dynamic styling and navigation

---
+++ Frontend/components/AdminHeader/AdminHeader.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const props = defineProps({
+  variant: {
+    type: String,
+    default: "default",
+    validator: (value) => ["default", "upload", "download", "delete"].includes(value as string),
+  },
+});
+
+const headerBG = computed(() => ({
+  'bg-base-100': props.variant === 'default',
+  'bg-info': props.variant === 'upload',
+  'bg-success': props.variant === 'download',
+  'bg-error': props.variant === 'delete',
+}));
+
+const colorText = computed(() => ({
+  'text-base-content': props.variant === 'default',
+  'text-info-content': props.variant === 'upload',
+  'text-success-content': props.variant === 'download',
+  'text-error-content': props.variant === 'delete',
+}));
+
+const goBack = () => {
+  router.back();
+};
+
+</script>
+
+<template>
+
+  <div
+      :class="headerBG"
+      class="w-full flex items-center p-4 gap-4 shadow z-10"
+  >
+
+    <CustomButton
+        :class="colorText"
+        color="default"
+        left-icon="fa-solid fa-arrow-left"
+        text=""
+        variant="ghost"
+        @click="goBack()"
+    />
+    <span
+        :class="colorText"
+        class="text-head bg-info-soft">Upload</span>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>

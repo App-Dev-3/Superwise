bb333096bb340d81e0d83671845c318f6fe4b6fa
Felix Teutsch <felix@teutsch.it>
2025-05-12 15:35:51 +0200

    feat(frontend): create Settings component

---
+++ Frontend/components/Settings/SettingsElement.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+
+  <div class="w-full flex flex-row">
+    <div class="flex flex-col w-full">
+      <span class="text-body">App Theme</span>
+      <span class="text-x-small">Choose your app theme</span>
+    </div>
+
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>

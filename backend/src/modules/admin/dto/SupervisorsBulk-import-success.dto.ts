10a28c40fe4e35aa73cbbc12ebf0801d1f0347d9
Mohammed Ammar Al Hamadani <mohammedamaar165@gmail.com>
2025-05-14 10:38:57 +0200

    feat(backend): add dto of bulk import (SCRUM-162)

---
+++ backend/src/modules/admin/dto/SupervisorsBulk-import-success.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SupervisorsBulkImportSuccessDto {
+  @ApiProperty({
+    description: 'Success flag',
+    example: true,
+  })
+  success: boolean;
+
+  @ApiProperty({
+    description: 'Success message',
+    example: 'Supervisors imported successfully',
+  })
+  message: string;
+
+  @ApiProperty({
+    description: 'Number of Supervisors imported',
+    example: 10,
+  })
+  supervisorsImported: number;
+}

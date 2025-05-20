4decf94d24dd714afdf1453808561826966ca9d3
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-05-21 00:59:54 +0200

    SCRUM-141: Nuxt FE(TEMP): Add navigation to dashboard so movement is possible
    THIS CHANGE NEEDS TO BE OVERWRITTEN BY THE DASHBOARD TASK -> its just a temp placeholder

--- Frontend/pages/supervisor/dashboard.vue
+++ Frontend/pages/supervisor/dashboard.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
     <!-- TODO: Add the content of the page -->
-    <div>TODO</div>
+    <div>
+        TODO
+        <BottomNav
+            :bottom-nav-buttons="bottomNavButtons"
+            :active-route="dummyRoute"
+            :always-show-labels="false"
+            @navigate="navigate"
+        />
+    </div>
+
 </template>
+
+<script setup lang="ts">
+
+
+
+const bottomNavButtons = [
+    { label: 'Dashboard', icon: 'house', route: '/supervisor/dashboard' },
+    { label: 'Matching', icon: 'user-group', route: '/supervisor/matching' },
+    { label: 'Chat', icon: 'message', route: '/supervisor/chat' }
+]
+
+
+
+function navigate(route: string) {
+    dummyRoute.value = route;
+    navigateTo(route);
+}
+
+const dummyRoute = ref("/");
+
+</script>
 
 <style scoped>
 

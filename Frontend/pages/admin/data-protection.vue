49f3e8332aa40f612a8c577c9df179931cf78429
Felix Teutsch <felix@teutsch.it>
2025-05-20 18:11:49 +0200

    feat(frontend): implement content structure and localization for data protection page

--- Frontend/pages/admin/data-protection.vue
+++ Frontend/pages/admin/data-protection.vue
@@ -1,12 +1,74 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
+const contentAdmin = [
+  "dataProtection.generic.generalInfoTitle",
+  "dataProtection.generic.generalInfoText",
+  "dataProtection.admin.generalInfoText",
+  "dataProtection.generic.collectedDataSubtitle",
+  "dataProtection.admin.collectedDataText",
+  "dataProtection.admin.collectedDataList",
+  "dataProtection.generic.changesAndOverwritesSubtitle",
+  "dataProtection.admin.changesAndOverwritesText",
+  "dataProtection.generic.dataDeletionSubtitle",
+  "dataProtection.admin.dataDeletionText",
+  "dataProtection.admin.dataRetentionSubtitle",
+  "dataProtection.admin.dataRetentionText.1",
+  "dataProtection.admin.dataRetentionList",
+  "dataProtection.admin.dataRetentionText.2",
+];
+
+const getStyle = (content: string) => {
+  if (content.match(/Subtitle(\.\d+)?$/))
+    return "pt-6";
+  // if (content.match(/Title(\.\d+)?$/))
+  //   return "";
+  // if (content.match(/text(\.\d+)?$/))
+  //   return "text-body";
+  // if (content.match(/List(\.\d+)?$/))
+  //   return "text-body list-disc list-inside";
+};
+
+const {t} = useI18n();
+
+const deleteData = () => {
+  // Implement the logic to delete data
+  console.log("Delete data button clicked");
+};
 </script>
 
 <template>
-    <!-- TODO: Add the content of the page -->
-    <div>TODO</div>
+  <div class="h-screen flex flex-col">
+    <AdminHeader
+      :header-text="$t('dataProtection.generic.pageHeader')"
+    />
+
+    <div class="px-6 py-8 flex flex-col gap-2 h-full overflow-y-auto">
+      <div
+        v-for="content in contentAdmin"
+        :key="content"
+        :class="getStyle(content)"
+      >
+        <ul v-if="content.match(/List(\.\d+)?$/)" class="list-disc pl-5">
+          <li v-for="(item, index) in t(content).split('\n')" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+
+
+        <h2 v-else-if="content.match(/Subtitle(\.\d+)?$/)" class="text-large">{{ $t(content) }}</h2>
+
+        <h1 v-else-if="content.match(/Title(\.\d+)?$/)" class="text-header">{{ $t(content) }}</h1>
+
+        <p v-else class="text-body">{{ $t(content) }}</p>
+      </div>
+      <CustomButton
+        :text="$t('dataProtection.generic.deleteMyData')"
+        block
+        class="py-8"
+        color="error"
+        left-icon="trash-can"
+        @click="() => deleteData()"
+      />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
--- Frontend/pages/supervisor/data-protection.vue
+++ Frontend/pages/supervisor/data-protection.vue
@@ -1,12 +1,72 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
+const contentStudentAndSupervisor = [
+  "dataProtection.generic.generalInfoTitle",
+  "dataProtection.generic.generalInfoText",
+  "dataProtection.generic.collectedDataSubtitle",
+  "dataProtection.student&supervisor.collectedDataText",
+  "dataProtection.student&supervisor.collectedDataList",
+  "dataProtection.generic.changesAndOverwritesSubtitle",
+  "dataProtection.student&supervisor.changesAndOverwritesText",
+  "dataProtection.generic.dataDeletionSubtitle",
+  "dataProtection.student&supervisor.dataDeletionText.1",
+  "dataProtection.student&supervisor.dataDeletionList",
+  "dataProtection.student&supervisor.dataDeletionText.2",
+];
+
+
+const getStyle = (content: string) => {
+  if (content.match(/Subtitle(\.\d+)?$/))
+    return "pt-6";
+  // if (content.match(/Title(\.\d+)?$/))
+  //   return "";
+  // if (content.match(/text(\.\d+)?$/))
+  //   return "text-body";
+  // if (content.match(/List(\.\d+)?$/))
+  //   return "text-body list-disc list-inside";
+};
+
+const {t} = useI18n();
+
+const deleteData = () => {
+  // Implement the logic to delete data
+  console.log("Delete data button clicked");
+};
 </script>
 
 <template>
-<!-- TODO: Add the content of the page -->
-    <div>TODO</div>
+  <div class="h-screen flex flex-col">
+    <AdminHeader
+      :header-text="$t('dataProtection.generic.pageHeader')"
+    />
+
+    <div class="px-6 py-8 flex flex-col gap-2 h-full overflow-y-auto">
+      <div
+        v-for="content in contentStudentAndSupervisor"
+        :key="content"
+        :class="getStyle(content)"
+      >
+        <ul v-if="content.match(/List(\.\d+)?$/)" class="list-disc pl-5">
+          <li v-for="(item, index) in t(content).split('\n')" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+
+
+        <h2 v-else-if="content.match(/Subtitle(\.\d+)?$/)" class="text-large">{{ $t(content) }}</h2>
+
+        <h1 v-else-if="content.match(/Title(\.\d+)?$/)" class="text-header">{{ $t(content) }}</h1>
+
+        <p v-else class="text-body">{{ $t(content) }}</p>
+      </div>
+      <CustomButton
+        :text="$t('dataProtection.generic.deleteMyData')"
+        block
+        class="py-8"
+        color="error"
+        left-icon="trash-can"
+        @click="() => deleteData()"
+      />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>

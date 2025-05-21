5aa5246d5ca72ae4c66a1fa141972be3141da13a
LionRise <lenastadlinger@gmail.com>
2025-05-21 22:16:29 +0200

    Revert "feature(frontend): modal function and base design done (SCRUM 139)"
    This reverts commit 4220c995559e6c20c15342eb4e00c010f68bde89.

--- Frontend/stores/useUserStore.ts
+++ Frontend/stores/useUserStore.ts
@@ -1,21 +1,17 @@
 import { defineStore } from 'pinia'
 import type { UserData } from '~/shared/types/userInterfaces'
-import { HttpMethods } from "#shared/enums/enums";
-
-const STORAGE_KEY = 'app_user'
+import {HttpMethods} from "#shared/enums/enums";
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    user: JSON.parse(localStorage.getItem(STORAGE_KEY) || 'null') as UserData | null,
+    user: null as UserData | null,
   }),
   actions: {
     setUser(userData: UserData) {
       this.user = userData
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(userData))
     },
     clearUser() {
       this.user = null
-      localStorage.removeItem(STORAGE_KEY)
     },
     // Keeps the user data in sync with the server. If the user is not logged in, it clears the user data.
     async refetchCurrentUser() {

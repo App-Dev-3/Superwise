b565e639e7dab64c28fa6bf49e89afa7b4d4c9ec
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-06-05 01:37:54 +0200

    SCRUM-168: Nuxt FE(feature): Create loading indicator component

---
+++ Frontend/components/LoadingIndicator/LoadingIndicator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full h-screen flex max-w-3xl m-auto flex-col p-8 justify-center items-center">
+    <ClientOnly>
+      <img
+        :src="
+          colorMode.value === 'dark'
+            ? '../images/appHeader_logo_dark.svg'
+            : '../images/appHeader_logo_light.svg'
+        "
+        alt="Logo image"
+        class="w-full max-w-xs"
+      >
+    </ClientOnly>
+    <span class="text-large flex flex-row items-center gap-1">
+      {{ props.text }}
+      <span class="loading loading-dots loading-xs translate-y-1" />
+    </span>
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: 'Loading...',
+  },
+});
+</script>

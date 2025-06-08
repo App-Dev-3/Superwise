6f665f113e72a488c14ccf62075e1d5fcb1ea634
FelixTeutsch <felix@teutsch.it>
2025-06-08 17:47:04 +0200

    feat(Frontend): use loading indicator component

--- Frontend/layouts/Default.vue
+++ Frontend/layouts/Default.vue
@@ -3,21 +3,9 @@
       v-if="isLoading"
       class="w-full h-screen flex max-w-xl m-auto flex-col p-8 justify-center items-center"
   >
-    <ClientOnly>
-      <img
-          :src="
-          colorMode.value === 'dark'
-            ? '../images/appHeader_logo_dark.svg'
-            : '../images/appHeader_logo_light.svg'
-        "
-          alt="Logo image"
-          class="w-full max-w-xs"
-      >
-    </ClientOnly>
-    <span class="text-large flex flex-row items-center gap-1">
-      Loading page
-      <span class="loading loading-dots loading-xs translate-y-1"/>
-    </span>
+    <LoadingIndicator
+        :text="t('generic.loading')"
+    />
   </div>
 
   <div v-else class="w-full h-screen flex max-w-xl m-auto flex-col">
@@ -28,9 +16,9 @@
 <script lang="ts" setup>
 import { until } from "@vueuse/core";
 import { onMounted } from "vue";
-import { useColorMode } from "#imports";
+import { useI18n } from "vue-i18n";
 
-const colorMode = useColorMode();
+const { t } = useI18n();
 
 const authStore = useAuthStore()
 await authStore.initialize()

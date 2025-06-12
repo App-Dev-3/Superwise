e6a5b4d825bdba353baf84c29334eba4baa1c745
FelixTeutsch <felix@teutsch.it>
2025-06-12 07:36:49 +0200

    feat(Frontend): fix minor styling mistakes. App tour is now working!!!

--- Frontend/components/Progress/ProgressBars.vue
+++ Frontend/components/Progress/ProgressBars.vue
@@ -10,12 +10,12 @@ const props = defineProps<ProgressBarProps>();
 </script>
 
 <template>
-  <div class="w-full h-fit gap-3">
+  <div class="w-full h-fit gap-3 flex flex-row">
     <div
         v-for="i in props.totalSteps"
         :key="i"
-        :class="{'bg-primary': props.currentStep >= i, 'bg-base-300': props.currentStep < i}"
-        class="h-3 w-full"
+        :class="{'bg-primary': props.currentStep >= i-1, 'bg-base-300': props.currentStep < i-1}"
+        class="h-3 w-full rounded-full"
     />
   </div>
 </template>

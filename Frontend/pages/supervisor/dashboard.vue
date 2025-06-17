834692aa187e99d1b0cbc3f1399af60bea639c43
None of your business <lenastadlinger@gmail.com>
2025-06-17 20:13:06 +0200

    fix(fe): toast for add student error

--- Frontend/pages/supervisor/dashboard.vue
+++ Frontend/pages/supervisor/dashboard.vue
@@ -7,8 +7,8 @@ import { useSupervisionRequests } from "~/composables/useSupervisionRequests";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import EmptyPagePlaceholder from "~/components/Placeholder/EmptyPagePlaceholder.vue";
 
-const authStore = useAuthStore()
-const { user } = storeToRefs(authStore)
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
 const { getUserByEmail } = useUserApi();
 const { getSupervisorByUserId } = useSupervisorApi();
 const userStore = useUserStore();
@@ -33,15 +33,15 @@ const visibleRequests = computed(
 );
 
 watch(
-    () => user.value?.primaryEmailAddress?.emailAddress,
-    async (email) => {
-      if (!email) return;
+  () => user.value?.primaryEmailAddress?.emailAddress,
+  async (email) => {
+    if (!email) return;
 
-      if (!userStore.user) {
-        const fetched = (await getUserByEmail(email)) as UserData;
-        userStore.setUser(fetched);
-      }
-      current_user.value = userStore.user;
+    if (!userStore.user) {
+      const fetched = (await getUserByEmail(email)) as UserData;
+      userStore.setUser(fetched);
+    }
+    current_user.value = userStore.user;
 
       if (current_user.value?.id) {
         const data = (await getSupervisorByUserId(
@@ -73,14 +73,14 @@ definePageMeta({
 <template>
   <div class="flex flex-col w-full gap-8 p-8">
     <ActionCard
-        :button-text="t('dashboard.supervisor.manageStudents')"
-        card-type="primary"
-        class="w-full text-center"
-        @action-button-clicked="navigate('/supervisor/currently-supervising')"
+      :button-text="t('dashboard.supervisor.manageStudents')"
+      card-type="primary"
+      class="w-full text-center"
+      @action-button-clicked="navigate('/supervisor/currently-supervising')"
     >
       <div class="py-8 px-16 w-full">
         <h2 class="text-xl">
-          <FontAwesomeIcon icon="user-group"/>
+          <FontAwesomeIcon icon="user-group" />
           {{
             // this works, even though the IDE tells you it doesnt. The frontend interface types are not consitent with the backend types. Dont ask why.
             (supervisor_data?.total_spots ?? 0) - (supervisor_data?.available_spots ?? 0)
@@ -93,31 +93,37 @@ definePageMeta({
     </ActionCard>
 
     <ActionCard
-        :button-text="t('generic.showAll')"
-        :disabled="visibleCount >= (pendingRequests?.length ?? 0)"
-        :header-text="t('generic.requests')"
-        card-type="ghost"
-        @action-button-clicked="navigateTo('/supervisor/matching')"
+      :button-text="t('generic.showAll')"
+      :disabled="visibleCount >= (pendingRequests?.length ?? 0)"
+      :header-text="t('generic.requests')"
+      card-type="ghost"
+      @action-button-clicked="navigateTo('/supervisor/matching')"
     >
       <EmptyPagePlaceholder
-          :render-condition="visibleRequests"
-          :text="t('dashboard.supervisor.noRequests')"
+        :render-condition="visibleRequests"
+        :text="t('dashboard.supervisor.noRequests')"
       />
       <NuxtLink
-          v-for="request in visibleRequests"
-          :key="request.id"
-          :to="`/profiles/${request.student.user_id}`"
+        v-for="request in visibleRequests"
+        :key="request.id"
+        :to="`/profiles/${request.student.user_id}`"
       >
         <MiniCard
-            :bottom-text="formatTimeString(request.updated_at, undefined)"
-            :first-name="request.student.user.first_name"
-            :image="request.student.user.profile_image || getPlaceholderImage(request.student.user.first_name, request.student.user.last_name) || ''"
-            :last-name="request.student.user.last_name"
-            :preview-text="request.student.thesis_description"
-            top-icon="message"
+          :bottom-text="formatTimeString(request.updated_at, undefined)"
+          :first-name="request.student.user.first_name"
+          :image="
+            request.student.user.profile_image ||
+            getPlaceholderImage(
+              request.student.user.first_name,
+              request.student.user.last_name
+            ) ||
+            ''
+          "
+          :last-name="request.student.user.last_name"
+          :preview-text="request.student.thesis_description"
+          top-icon="message"
         />
       </NuxtLink>
-
     </ActionCard>
   </div>
 </template>

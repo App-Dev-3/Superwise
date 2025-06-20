eec92e4af44472ff2c0503e6aaa724e92bb9e16c
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2025-06-20 21:04:24 +0200

    feat(frontend): create progress thing and add search for admin (Scrum-313) (#206)
    Co-authored-by: FelixTeutsch <felix@teutsch.it>

---
+++ Frontend/components/Progress/Progress.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+
+interface ProgressProps {
+  progress: number;
+  text: string;
+}
+
+const props = defineProps<ProgressProps>();
+
+</script>
+
+<template>
+  <div class="w-full h-fit flex flex-row p-6 gap-6 items-center border-b border-b-base-300">
+    <div>
+      <div
+          :style="'--value:'+props.progress+';'" aria-valuenow="70"
+          class="radial-progress bg-success text-success-content border-success border-4"
+          role="progressbar"
+      >
+        {{ props.progress }}%
+      </div>
+    </div>
+    <p class="text-body opacity-75">
+      {{ props.text }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- Frontend/middleware/01.routing.global.ts
+++ Frontend/middleware/01.routing.global.ts
@@ -1,101 +1,101 @@
-import {UserRoles} from "#shared/enums/enums";
-import {until} from "@vueuse/core";
+import { UserRoles } from "#shared/enums/enums";
+import { until } from "@vueuse/core";
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  // ============ USER IS NOT AUTHENTICATED YET ============
+    // ============ USER IS NOT AUTHENTICATED YET ============
 
-  // Skip if middleware is running on the server
-  if (import.meta.server) return
+    // Skip if middleware is running on the server
+    if (import.meta.server) return
 
-  const authStore = useAuthStore()
-  await authStore.initialize()
-  const { user, isSignedIn, isLoaded } = storeToRefs(authStore)
-  if (!isLoaded.value) {
-    await until(isLoaded).toBe(true)
-  }
-  const registrationStore = useRegistrationStore()
-
-  // Only allow access to public paths if not signed in
-  const publicPaths = ['/']
-
-  if (!isSignedIn.value) {
-    if (!publicPaths.includes(to.path)) {
-      return navigateTo('/')
+    const authStore = useAuthStore()
+    await authStore.initialize()
+    const { user, isSignedIn, isLoaded } = storeToRefs(authStore)
+    if (!isLoaded.value) {
+        await until(isLoaded).toBe(true)
     }
-    return
-  }
+    const registrationStore = useRegistrationStore()
 
-  const userEmail = user.value?.primaryEmailAddress?.emailAddress;
-  if (!userEmail) {
-    return navigateTo('/')
-  }
-  // ============ USER IS NOW SIGNED IN ============
+    // Only allow access to public paths if not signed in
+    const publicPaths = [ '/' ]
 
-  // Register user if is not already registered
-
-  if (!registrationStore.status || !registrationStore.status.exists || !registrationStore.status.is_registered) {
-    await registrationStore.fetchRegistrationStatus(userEmail)
-  }
-  let userRole = UserRoles.STUDENT
-  if (registrationStore.status?.role) {
-    userRole = registrationStore.status.role
-  }
-
-  const accountRegistrationCompleted = registrationStore.status?.is_registered || false
-
-  if (userRole === UserRoles.ADMIN) {
-    if (!accountRegistrationCompleted) {
-      if (!to.path.startsWith('/admin/app-tour')) {
-        return navigateTo('/admin/app-tour')
-      }
+    if (!isSignedIn.value) {
+        if (!publicPaths.includes(to.path)) {
+            return navigateTo('/')
+        }
+        return
     }
-    if (!to.path.startsWith('/admin')) {
-      return navigateTo('/admin/dashboard')
+
+    const userEmail = user.value?.primaryEmailAddress?.emailAddress;
+    if (!userEmail) {
+        return navigateTo('/')
     }
-    return
-  }
+    // ============ USER IS NOW SIGNED IN ============
 
-  const tagsSelected = registrationStore.status?.tags || false
+    // Register user if is not already registered
 
-  if (!accountRegistrationCompleted && to.path.startsWith('/onboarding')) {
-    return
-  }
-
-  if (!accountRegistrationCompleted && !to.path.startsWith('/onboarding')) {
-    if (userRole === UserRoles.SUPERVISOR) {
-      return navigateTo('/onboarding/supervisor')
-    } else {
-      return navigateTo('/onboarding/student')
+    if (!registrationStore.status || !registrationStore.status.exists || !registrationStore.status.is_registered) {
+        await registrationStore.fetchRegistrationStatus(userEmail)
     }
-  }
-  if (
-    accountRegistrationCompleted && 
-    !tagsSelected
-  ) {
-    if (!to.path.startsWith(`/onboarding/${userRole.toLowerCase()}`)) {
-      return navigateTo(`/onboarding/${userRole.toLowerCase()}`)
+    let userRole = UserRoles.STUDENT
+    if (registrationStore.status?.role) {
+        userRole = registrationStore.status.role
     }
-    return
-  }
-  if (accountRegistrationCompleted && tagsSelected && to.path.startsWith('/onboarding')) {
-    return navigateTo(`/${userRole.toLowerCase()}/dashboard`)
-  }
-  
-  // ============ USER IS NOW REGISTERED AND ONBOARDED ============
 
-  // make sure only each role-specific route can only be accessed by the corresponding role
-  if (
-      !to.path.startsWith(`/${userRole.toLowerCase()}`)
-      && !to.path.startsWith('/profiles')  && to.path !== '/playground'
-  ) {
-    return navigateTo(`/${userRole.toLowerCase()}/dashboard`)
-  }
+    const accountRegistrationCompleted = registrationStore.status?.is_registered || false
 
-  // Redirect signed-in users away from the root
-  if (to.path === '/') {
-    if (userRole) {
-      return navigateTo(`/${userRole.toLowerCase()}/dashboard`)
+    if (userRole === UserRoles.ADMIN) {
+        if (!accountRegistrationCompleted) {
+            if (!to.path.startsWith('/admin/app-tour')) {
+                return navigateTo('/admin/app-tour')
+            }
+        }
+        if (!to.path.startsWith('/admin') && !to.path.startsWith('/profiles')) {
+            return navigateTo('/admin/dashboard')
+        }
+        return
     }
-  }
+
+    const tagsSelected = registrationStore.status?.tags || false
+
+    if (!accountRegistrationCompleted && to.path.startsWith('/onboarding')) {
+        return
+    }
+
+    if (!accountRegistrationCompleted && !to.path.startsWith('/onboarding')) {
+        if (userRole === UserRoles.SUPERVISOR) {
+            return navigateTo('/onboarding/supervisor')
+        } else {
+            return navigateTo('/onboarding/student')
+        }
+    }
+    if (
+        accountRegistrationCompleted &&
+        !tagsSelected
+    ) {
+        if (!to.path.startsWith(`/onboarding/${ userRole.toLowerCase() }`)) {
+            return navigateTo(`/onboarding/${ userRole.toLowerCase() }`)
+        }
+        return
+    }
+    if (accountRegistrationCompleted && tagsSelected && to.path.startsWith('/onboarding')) {
+        return navigateTo(`/${ userRole.toLowerCase() }/dashboard`)
+    }
+
+    // ============ USER IS NOW REGISTERED AND ONBOARDED ============
+
+    // make sure only each role-specific route can only be accessed by the corresponding role
+    if (
+        !to.path.startsWith(`/${ userRole.toLowerCase() }`)
+        && !to.path.startsWith('/profiles') && to.path !== '/playground'
+    ) {
+        return navigateTo(`/${ userRole.toLowerCase() }/dashboard`)
+    }
+
+    // Redirect signed-in users away from the root
+    if (to.path === '/') {
+        if (userRole) {
+            return navigateTo(`/${ userRole.toLowerCase() }/dashboard`)
+        }
+    }
 })
 
